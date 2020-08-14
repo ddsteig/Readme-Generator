d168e65415d770580f8b32694ac87e507771e746
Danny Steiger <Tekhexrax@yahoo.com>
2020-08-14 18:57:58 -0400

    Testing generator

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Readme-Generator
-A readme generator run through node.
+# Readme Generator
+
+      <hr>
+
+      '## Description
+
+      A program used to generate a basic readme through node.
+
+      <hr>
+
+      ## Table of Contents
+
+      <hr>
+
+      ## Installaion Instructions
+
+      Run nmp i, Run node on the index.js, input prompt info.
+
+      <hr>
+
+      ## Usage
+
+      undefined
+
+      <hr>
+
+      ## Contributers
+
+      Me myself and I
+
+      <hr>
+
+      ## Tests
+
+      undefined
+
+      ## Questions
+
+      <p>For any questions, contact me at the following: </p>
+
+      Email: blah@blah.com
+
+      Github: ddsteig
+
+      
--- index.js
+++ index.js
@@ -1,13 +1,106 @@
+var fs = require("fs");
 var inquirer = require("inquirer");
 
 inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "What is the title of your readme?",
-            name: "title"
-        }
-    ])
-    .then (function(response){
-        console.log(response.title)
-    })
+  .prompt([
+    {
+      type: "input",
+      message: "What is the title of your readme?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Enter a description of your readme.",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter installation instructions.",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Enter useage information.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Select a license.",
+      name: "select",
+    },
+    {
+      type: "input",
+      message: "Enter contributer, or resource information.",
+      name: "contributer",
+    },
+    {
+      type: "input",
+      message: "List any tests",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Enter your email.",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Enter your GitHub user name.",
+      name: "github",
+    },
+  ])
+  .then(function (response) {
+    let readme = `# ${response.title}
+
+      <hr>
+
+      '## Description
+
+      ${response.description}
+
+      <hr>
+
+      ## Table of Contents
+
+      <hr>
+
+      ## Installaion Instructions
+
+      ${response.install}
+
+      <hr>
+
+      ## Usage
+
+      ${response.useage}
+
+      <hr>
+
+      ## Contributers
+
+      ${response.contributer}
+
+      <hr>
+
+      ## Tests
+
+      ${response.test}
+
+      ## Questions
+
+      <p>For any questions, contact me at the following: </p>
+
+      Email: ${response.email}
+
+      Github: ${response.github}
+
+      `;
+
+    fs.writeFile("README.md", readme, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Readme Generated!");
+      }
+    });
+  });
